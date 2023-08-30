c648363db0580712a9d0710ad359bf407db372cc
j3lte <jwlagendijk@gmail.com>
2023-08-30 19:40:34 +0200

    Compile script

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,4 +1,4 @@
-deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-unknown-linux-gnu --output  x86_64-unknown-linux-gnu-mx-widget-tooling cli.ts
-deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target aarch64-apple-darwin --output  aarch64-apple-darwin-mx-widget-tooling cli.ts
-deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-apple-darwin --output  x86_64-apple-darwin-mx-widget-tooling cli.ts
-deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-pc-windows-msvc --output  x86_64-pc-windows-msvc-mx-widget-tooling cli.ts
+deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-unknown-linux-gnu --output x86_64-unknown-linux-gnu-mx-widget-tooling cli.ts
+deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target aarch64-apple-darwin --output aarch64-apple-darwin-mx-widget-tooling cli.ts
+deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-apple-darwin --output x86_64-apple-darwin-mx-widget-tooling cli.ts
+deno compile --allow-run --allow-read --allow-net --allow-write --allow-env --target x86_64-pc-windows-msvc --output x86_64-pc-windows-msvc-mx-widget-tooling cli.ts
