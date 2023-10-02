70fce4082801bbe1f1012b409d73f47f9e5e2518
j3lte <jwlagendijk@gmail.com>
2023-10-02 11:20:55 +0200

    Update deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,5 +1,5 @@
 // Copyright 2023 J.W. Lagendijk. All rights reserved. MIT license.
 
-export { stripAnsiCode } from "https://deno.land/std@0.201.0/fmt/colors.ts";
-export { walk } from "https://deno.land/std@0.201.0/fs/walk.ts";
-export { globToRegExp } from "https://deno.land/std@0.201.0/path/glob.ts";
+export { stripAnsiCode } from "https://deno.land/std@0.203.0/fmt/colors.ts";
+export { walk } from "https://deno.land/std@0.203.0/fs/walk.ts";
+export { globToRegExp } from "https://deno.land/std@0.203.0/path/glob.ts";
